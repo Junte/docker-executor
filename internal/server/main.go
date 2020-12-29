89837e89c690aff7da6eed7f7c07dbb925db2156
Viacheslav Riazanov <riazanovslv@gmail.com>
2020-12-29 14:12:40 +0300

    Add a draft realization #1

---
+++ internal/server/main.go
@@ -0,0 +1,50 @@
+package server
+
+import (
+	"executor/internal/core"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func Run() {
+	log.Printf("version: %s", core.Version)
+
+	core.ReadConfig()
+
+	http.HandleFunc("/", handler)
+
+	err := startServer()
+	if err != nil {
+		log.Fatal("failed start server: ", err)
+	}
+}
+
+func startServer() (err error) {
+	log.Printf("starting http server on port %s", core.Config.Port)
+	err = http.ListenAndServe(core.Config.Port, nil)
+	return
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		http.Error(w, "", http.StatusMethodNotAllowed)
+		return
+	}
+
+	if err := r.ParseForm(); err != nil {
+		http.Error(w, fmt.Sprintf("wrong query params err: %v", err), http.StatusBadRequest)
+		return
+	}
+
+	args := make(map[string]string)
+	for key, values := range r.Form {
+		args[key] = values[0]
+	}
+
+	err := core.ExecuteComponent(r.FormValue("component"), r.FormValue("key"), args)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("deploy err: %v", err), http.StatusBadRequest)
+		return
+	}
+}
